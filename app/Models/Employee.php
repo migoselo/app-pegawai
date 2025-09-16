20646dcbe8acea9b5cac5172c830070fe3d0b3b3
migoselo <putridheerie@gmail.com>
2025-09-16 20:56:59 +0700

    penambahan migration dan model employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
 class Employee extends Model
 {
-    //
+    protected $fillable = [
+        'nama_lengkap',
+        'email',
+        'nomor_telepon',
+        'tanggal_lahir',
+        'alamat',
+        'tanggal_masuk',
+        'status',
+    ];
+    
 }
--- database/migrations/2025_09_10_131525_create_employees_table.php
+++ database/migrations/2025_09_10_131525_create_employees_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_lengkap', 100);
+            $table->string('email', 100);
+            $table->string('nomor_telepon', 15);
+            $table->date('tanggal_lahir');
+            $table->text('alamat');
+            $table->date('tanggal_masuk');
+            $table->enum('status', ['aktif', 'nonaktif'])->default('aktif');
             $table->timestamps();
         });
     }
